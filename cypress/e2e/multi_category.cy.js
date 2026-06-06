f556dc0a1f9c8f075adc1a09ff3f4dbe7ca27e50
Andrea Mauro <andrea.mauro.h@gmail.com>
2026-06-06 23:22:00 +0200

    feat: make category a set
    close #3

---
+++ cypress/e2e/multi_category.cy.js
@@ -0,0 +1,56 @@
+describe('Multi-Category Flashcards', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:5566');
+        // Ensure we are on the flashcards tab
+        cy.contains('button', 'Flashcards').click();
+    });
+
+    it('should show words assigned to multiple categories in both filters', () => {
+        // "Bag" is assigned to ["basic", "clothing"]
+        
+        // Check in Basic
+        cy.get('.category-btn').contains('Basic').click();
+        cy.get('#flashcardContainer').contains('Bag').should('be.visible');
+
+        // Check in Clothing
+        cy.get('.category-btn').contains('Clothing').click();
+        cy.get('#flashcardContainer').contains('Bag').should('be.visible');
+
+        // "Water" is assigned to ["eating", "nature"]
+        
+        // Check in Eating
+        cy.get('.category-btn').contains('Eating').click();
+        cy.get('#flashcardContainer').contains('Water').should('be.visible');
+
+        // Check in Nature
+        cy.get('.category-btn').contains('Nature').click();
+        cy.get('#flashcardContainer').contains('Water').should('be.visible');
+
+        // Check in House
+        cy.get('.category-btn').contains('House').click();
+        cy.get('#flashcardContainer').contains('Bathroom').should('be.visible');
+
+        // Check in Bathroom
+        cy.get('.category-btn').contains('Bathroom').click();
+        cy.get('#flashcardContainer').contains('Bathroom').should('be.visible');
+    });
+
+    it('should display multiple categories on the card', () => {
+        cy.get('.category-btn').contains('Basic').click();
+        cy.contains('.flashcard', 'Bag').within(() => {
+            cy.get('.card-category').invoke('text').then((text) => {
+                expect(text.toLowerCase()).to.include('basic');
+                expect(text.toLowerCase()).to.include('clothing');
+            });
+        });
+    });
+
+    it('should filter correctly for single category words', () => {
+        // "Bad" is ["basic"]
+        cy.get('.category-btn').contains('Clothing').click();
+        cy.get('#flashcardContainer').contains('Bad').should('not.exist');
+
+        cy.get('.category-btn').contains('Basic').click();
+        cy.get('#flashcardContainer').contains('Bad').should('be.visible');
+    });
+});
